e29225b808633183093616541d4a7e1afd7674b6
Shrey1528 <103505972+Shrey1528@users.noreply.github.com>
2022-05-25 17:44:31 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// Globals
+// __dirname - path to current directory
+// __filename - filename
+// require
+// module
+// process - about the environment where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
